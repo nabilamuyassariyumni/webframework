8986c8003b883196f6d71644b2d68cc9b8eff8bc
Nabila Muyassari Yumni <nabilamuyassariyumni@gmail.com>
2026-04-07 15:28:59 +0700

    Update MahasiswaController.php

--- app/Http/Controllers/Auth/MahasiswaController.php
+++ app/Http/Controllers/Auth/MahasiswaController.php
@@ -4,11 +4,16 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class MahasiswaController extends Controller
 {
     public function index()
     {
         return "Ini adalah index di MahasiswaController";
+    }
+
+    public function insertSql(){
+        $query = DB::insert("INSERT INTO students(nim, nama_lengkap, tempat_lahir, tgl_lahir, email, prodi, alamat, created_at, updated_at) VALUES ('2401092010', 'Linus B Torvalds', 'Finlandia', '1971-08-12', 'linus@linux.org', 'TRPL', 'Jl. Sudirman no.10 Padang', now(), now())");
     }
 }
